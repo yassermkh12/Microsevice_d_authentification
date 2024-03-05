cab7c3b1938d91821a62a96f7a3b27d689b398c0
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2024-03-05 20:15:11 +0100

    20:15 security config

--- microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/securityConfig/SecurityConfig.java
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/securityConfig/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.microserviceAuthentification.security.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,16 +15,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .rememberMe(Customizer.withDefaults());
+                http
+                        .csrf(csrf -> csrf
+                                .disable() // Désactiver CSRF
+                        )
+                        .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/api/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/save-users").permitAll()
+                            .anyRequest().permitAll()
+                            )
+                        .formLogin(formLogin -> formLogin
+                            .loginPage("/login")
+                            .permitAll()
+                        )
+
+                        .rememberMe(Customizer.withDefaults());
 
         return http.build();
     }
