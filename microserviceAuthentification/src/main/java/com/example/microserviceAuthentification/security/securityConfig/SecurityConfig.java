f5bbe82601835b8134019ec859da538c5eaa88d7
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2024-03-05 19:03:01 +0100

    19:02 security config

---
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/securityConfig/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.microserviceAuthentification.security.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
