e9cb15965477e417f037d2c590e92e70f2a63cdd
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2024-03-07 16:08:54 +0100

    16:08 jwt filter

---
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.example.microserviceAuthentification.security.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+    }
+}
