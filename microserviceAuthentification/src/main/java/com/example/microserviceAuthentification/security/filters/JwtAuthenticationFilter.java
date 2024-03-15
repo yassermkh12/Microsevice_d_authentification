097eee78c4dac6e3dfaa51761c2814b9b4b45f3d
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2024-03-15 13:30:14 +0000

    13:30 Jwt filter

--- microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/filters/JwtAuthenticationFilter.java
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/filters/JwtAuthenticationFilter.java
@@ -8,6 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,13 +23,20 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtService jwtService;
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+
+        log.info("*** le filtre internale commence ***");
+
+//        log.info("la requette est : "+ request.getHeader());
         final String authHeader = request.getHeader("Authorization");
+        log.info("le header d authentification : "+ authHeader);
         final String jwt;
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
@@ -31,6 +44,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         String username = jwtService.extractUsername(jwt);
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(jwt,userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                log.info("le header d authentificationToken : "+ authenticationToken);
 
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//                log.info("llll" + SecurityContextHolder.getContext().setAuthentication(authenticationToken));
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/securityConfig/SecurityConfig.java
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/security/securityConfig/SecurityConfig.java
@@ -1,49 +1,77 @@
 package com.example.microserviceAuthentification.security.securityConfig;
 
+import com.example.microserviceAuthentification.security.filters.JwtAuthenticationFilter;
 import com.example.microserviceAuthentification.security.repositories.IUserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
     @Autowired
     private IUserRepository userRepository;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-                http.csrf(csrf -> csrf
+                http
+                        .csrf(csrf -> csrf
                                 .disable() // Désactiver CSRF
                         )
                         .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/auth/authenticate").permitAll()
 //                        .requestMatchers(HttpMethod.POST, "/save-users").permitAll()
-                                        .anyRequest().permitAll()
+                                        .anyRequest().authenticated()
                         )
-                        .formLogin(formLogin -> formLogin
-                                .loginPage("/login")
-                                .permitAll()
+                        .sessionManagement(session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         )
-
-                        .rememberMe(Customizer.withDefaults());
-
-
+                        .authenticationProvider(authenticationProvider)
+//                        .formLogin(formLogin -> formLogin
+//                                .loginPage("/login")
+//                                .permitAll()
+//                        )
+                        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        log.info("**************************** user details service ************************");
-//        log.info("le username est : " + username -> userRepository.findByUserName(username));
-        return username -> userRepository.findByUserName(username);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        log.info("**************************** user details service ************************");
+////        log.info("le username est : " + username -> userRepository.findByUserName(username));
+//        return username -> userRepository.findByUserName(username);
+//    }
+//    @Bean
+//    public AuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return daoAuthenticationProvider;
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 }
