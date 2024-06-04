cd55708c8441b4d4a33d9053be602acd93b0460c
yassermkh12 <mokhtariyasser3@gmail.com>
2024-06-04 14:34:34 +0100

    14:34 04/06/2024 add role automaticly

--- microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/MicroserviceAuthentificationApplication.java
+++ microserviceAuthentification/src/main/java/com/example/microserviceAuthentification/MicroserviceAuthentificationApplication.java
@@ -1,5 +1,9 @@
 package com.example.microserviceAuthentification;
 
+import com.example.microserviceAuthentification.security.entities.Role;
+import com.example.microserviceAuthentification.security.repositories.IRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +11,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class MicroserviceAuthentificationApplication {
+public class MicroserviceAuthentificationApplication implements CommandLineRunner {
+
+	@Autowired
+	private IRoleRepository roleRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceAuthentificationApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (roleRepository.count() == 0) {
+			Role adminRole = new Role(1L,"ADMIN");
+			Role userRole = new Role(2L,"USER");
+			roleRepository.save(adminRole);
+			roleRepository.save(userRole);
+		}
+	}
 }
